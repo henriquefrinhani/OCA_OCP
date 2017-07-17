6c36af57e8d9ff41a88a3a0aae5057133fd7821f
henriquefrinhani <henriquefrinhani@hotmail.com>
2017-07-17 17:18:55 -0300

    metodo visitFile

--- src/com/ocp/capitulo_9/SimpleFileVisitorTeste.java
+++ src/com/ocp/capitulo_9/SimpleFileVisitorTeste.java
@@ -9,17 +9,21 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
 public class SimpleFileVisitorTeste extends SimpleFileVisitor<Path> {
-	
-	public FileVisitResult visitorFile(Path file, BasicFileAttributes atts) throws IOException{
-		if(file.getFileName().endsWith(".txt"))
-			Files.delete(file);
+
+	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+		if ( file.getFileName().endsWith("\\.txt")){
+			System.out.println("File: " + file);
+		}
+		//System.out.println("File.getName: "+file.getFileName());
+		//System.out.println("File.getName: "+file.getFileName().endsWith("txt"));
 		return FileVisitResult.CONTINUE;
 	}
-	
-	public static void main(String[] args)throws IOException{
+
+	public static void main(String[] args) throws IOException {
 		SimpleFileVisitorTeste dirs = new SimpleFileVisitorTeste();
-		
 		Files.walkFileTree(Paths.get("c:\\temp"), dirs);
+		System.out.println("Fim");
+
 	}
 
 }
