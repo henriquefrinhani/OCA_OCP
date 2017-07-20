2bf1a0f2ecb8ce1cb87a5dc7cc13a035b0edb111
henriquefrinhani <henriquefrinhani@hotmail.com>
2017-07-20 16:57:40 -0300

    mudanças no teste

--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -1,26 +1,14 @@
 package teste;
 
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 
-public class Teste extends SimpleFileVisitor<Path>{
-	
-	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException{
-		
-		if(file.getFileName().toString().endsWith(".txt"))
-			System.out.println(file);
-		
-		return FileVisitResult.CONTINUE;
-	}
-	
-	public static void main(String[] args) throws IOException {
-		Teste dirs = new Teste();
-		
-		Files.walkFileTree(Paths.get("d:/usuarios/t040819421"), dirs);
+public class Teste {
+	public static void main(String[] args) {
+		try {
+			Files.deleteIfExists(Paths.get("c:/temp/documento.doc"));
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 }
